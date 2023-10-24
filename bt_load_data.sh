c961a54493319a02c940e86614f3b7923dfab6d4
Jeronimo Rodriguez <jeronimo.rodriguez@temple.edu>
2023-10-24 13:52:42 -0400

    fix .sh and see if this shit finally runs

--- bt_load_data.sh
+++ bt_load_data.sh
@@ -1,8 +1,3 @@
-## Download docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
 
 ### Download compressed datasets
 # Species - Biomod
